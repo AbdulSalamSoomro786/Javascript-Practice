3cdb8a802b9fb78f469940a212edb9fd2ba78668
Abdul Salam Soomro <salmankhan7778666@gmail.com>
2018-02-23 18:55:39 +0500

    Javascript practice

---
+++ app.js
@@ -0,0 +1,481 @@
+
+
+
+
+
+// // // // //                 // (   Alert  )
+
+
+// // // // //                 // Task - 1
+
+// // // // //                 // alert ("Welcome to Js Land");
+
+// // // // //                 // Task - 2
+
+// // // // //                 // alert ("Welcome to JS Lane" '\n' "Happy coding!");
+
+// // // // //                 // Task - 3
+
+// // // // //                 // alert ("Welcome to Js land . . . ");
+
+
+// // // // //                 // Task - 4 
+
+
+// // // // //                 //   Done in Browsers console log !
+
+
+
+// // // // //                 //   (       Variables for string        )
+
+
+
+// // // // //                 //              Task - 1 
+
+// // // // //                 //   var username;
+
+// // // // //                 //   Task - 2
+
+// // // // //                 //   var myname = "Abdul Salam Soomro";
+
+
+// // // // //                 //   Task - 3
+
+// // // // //                 //   var mesaage;
+
+// // // // //                 //   Task - 4
+
+// // // // //                 //   mesaage = "Hello world!";
+
+// // // // //                 //   alert (message);
+
+
+// // // // //                 //   Task - 5
+
+// // // // //                 // var name = "Abdul Salam";
+
+// // // // //                 // var cast = "Soomro";
+
+// // // // //                 // var age = "24";
+
+// // // // //                 // alert(name + cast + age);
+
+
+// // // // //                 // Task - 6
+
+// // // // //                 // var b = "pizza";
+
+// // // // //                 // alert (b + \n + "pizz" \n + "piz" + \n +"piz" + \n + "p");
+
+
+// // // // //                 // Task - 7
+
+
+// // // // //                 // var job = "developer";
+
+// // // // //                 // var kids = "5";
+
+// // // // //                 // var partner = "Suhail";
+
+// // // // //                 // alert(job + kids + partner);
+
+
+// // // // //                 // Task - 8
+
+// // // // //                 // var email;
+
+// // // // //                 // email = "abc@gmail.com";
+
+// // // // //                 // alert ("My Email address is " + email);
+
+
+// // // // //                 // Task - 9
+
+// // // // //                 // var book = "A smarter way to learn javascript";
+
+// // // // //                 // alert ("i am trying to learn javascript from " + book);
+
+
+// // // // //                 // Task - 10
+
+                
+// // // // //                 // var job = "developer";
+
+// // // // //                 // var kids = "5";
+
+// // // // //                 // var partner = "Suhail";
+
+// // // // //                 // alert(job + kids + partner);
+
+
+
+
+
+
+// // // // //                 // Task - 11
+
+
+// // // // //                 // var a = "Hello world ! ";
+
+// // // // //                 // document.write (a);
+
+
+
+
+// // // // //                 //                 (     VARIABLES       FOR      NUMBERS    )
+
+
+// // // // //                 //                 Task - 1
+
+
+// // // // //                 //                 var age = "i am 14 years old";
+
+// // // // //                 //                 alert (age);
+
+
+// // // // //                 //                 Task - 2
+
+
+// // // // //                 //                 var vist = 33;
+
+// // // // //                 //                 alert ("you have visited this site " + vist + " Times");
+
+
+// // // // //                 //                 Task - 3
+
+
+// // // // //                 //                 var birth = "my birth year is 99";
+
+// // // // //                 //                 document.write(birth);
+
+                                
+                                
+// // // // //                 //                 Task - 4 
+
+
+// // // // //                 //                 var name = "Raza orderd ";
+
+// // // // //                 //                 var product = "Suit ";
+
+// // // // //                 //                 var quantity = "55";
+
+// // // // //                 //                 document.write(name + product + quantity);
+
+
+// // // // //                 //                 Task - 5
+
+
+
+
+
+
+
+// // // // //                 //                                 (    VARIABLE Names legal @ Illagel    )
+
+
+// // // // //                 //                                 Task - 1
+
+
+// // // // //                 //                                 var _$;
+
+// // // // //                 //                                 var $_;
+
+// // // // //                 //                                 var abdulSalam;
+
+
+
+// // // // //                 //                                 Task - 2
+
+
+// // // // //                 //                                 var rules = "Rules for naming JS variables ";
+
+// // // // //                 //                                 var a = "Variable names can only contains ; $sm , camelCase and _$ ";
+
+// // // // //                 //                                 var b = "Variables must begin with a Undescroe - Dollar sign @ Camel case !";
+
+// // // // //                 //                                 var c = "Variable names are case sensitive !";
+
+// // // // //                 //                                 var d = "Variable names should not be JS keyword !";
+
+// // // // //                 //                                 document.write (rules + \n + a + \n + b + \n + c + \n + d);
+
+
+
+// // // // //                 //                                                      (   Math Expressions  )
+
+
+
+
+
+// // // // //                 //                                                      Task - 1
+
+
+// // // // //                 //                                                      var sum = " Sum of 13 + 9 is "
+
+// // // // //                 //                                                      var num = 13 + 9;
+
+// // // // //                 //                                                      alert (sum + num);
+
+
+                                                                     
+
+// // // // //                 //                                                      Task - 2
+
+
+
+// // // // //                 //                                                      var sum = "result of substracting of 9 from 13 is  "
+
+// // // // //                 //                                                      var num = 13 - 9;
+
+// // // // //                 //                                                      alert (sum + num);
+
+
+// // // // //                 //                                                      Task - 3
+
+
+// // // // //                 //                                                      var sum = " Result of multiplying 13 and 9 is  "
+
+// // // // //                 //                                                      var num = 13 * 9;
+
+// // // // //                 //                                                      alert (sum + num);
+
+
+
+
+// // // // //                 //                                                      Task - 4 
+
+
+
+// // // // //                 //                                                      var sum = "result of dividng 13 and 9 is  "
+
+// // // // //                 //                                                      var num = 13 / 9;
+
+// // // // //                 //                                                      alert (sum + num);
+
+
+// // // // //                 //                                                      Task - 5 
+
+
+
+// // // // //                 //                                                      var sum = "Remainder of dividng 13 and 9 is "
+
+// // // // //                 //                                                      var num = 13 % 9;
+
+// // // // //                 //                                                      alert (sum + num);
+
+
+// // // // //                 //                                                      Task - 6
+
+
+
+
+
+
+
+// // // // //       Task - 7
+
+
+
+// // // // // var color_td;
+// // // // // document.write("<table border='1px'>");
+
+// // // // // for(var i = 1; i < 10; i++) {
+
+// // // // // 	document.write("<tr style='height:30px;'>");
+
+// // // // // 	for(var j = 1; j < 10; j++) {
+
+// // // // // 		if(j == 1 || i == 1) {
+// // // // // 			color_td = "#ccc";
+// // // // // 		}
+// // // // // 		else {
+// // // // // 			color_td = "#fff";
+// // // // // 		}
+
+// // // // // 		document.write("<td style='width:30px;background-color:" + color_td + "'>" + i*j + "</td>");
+// // // // // 	}
+// // // // // 	document.write("</tr>");
+// // // // // }
+
+// // // // // document.write("</table>");
+
+
+// // // // //      Task - 8 
+
+
+
+                           
+// // // // var price = "Price of item 1 ";
+
+// // // // var prc = 560;
+
+// // // // var price2 = "Price of item 2 is ";
+
+// // // // var prc2 = 200;
+
+// // // // var shipping = "shipping charges ";
+
+// // // // var shipping2 = 150;
+
+// // // // var total = "Total cost of your ordered is  ";
+
+// // // // var cost  = prc + prc2 + shipping;
+
+// // // // document.write(price  + prc + price2 + prc2 + shipping + shipping2 + total + cost );
+
+
+
+
+// // // //        Task - 9 
+
+
+
+
+
+
+
+
+
+
+// // //                              (   User input & Condition statement   )
+
+
+// // //                              Task - 1 
+
+
+// // //                              var user = prompt("Please enter your name","Abdul Salam");
+
+// // //                              var input = "Abdul salam";
+
+// // //                              if ( user === input){
+// // //                                  alert("hi Abdul salam");
+// // //                              }
+
+
+
+// // //                                 Task - 2
+
+
+// // //                                 var v = prompt ("Enter city name ?");
+
+// // //                                 if ( v === "karachi"){
+// // //                                     alert("city of light`s")
+// // //                                 }
+
+
+
+
+// // //                                    Task  -  3
+
+
+// // //                                    var gender = prompt("Enter gender");
+                                   
+// // //                                    if (gander === "male"){
+// // //                                        alert("Good morning sir")
+// // //                                     ;
+// // //                                    }
+
+// // //                                    if (gender === "female"){
+// // //                                        alert("Good morning mam");
+// // //                                    }
+
+
+
+
+
+// // //                                          Task - 4 
+
+
+
+// // //                                          var petrol = prompt("Enter fuel number ? ");
+
+// // //                                          if (petrol <= 0.25){
+// // //                                              alert("Please refill fuel");
+// // //                                          }
+
+
+
+
+
+// // //                                              Task - 5 
+
+
+
+
+// // //                                              var sub = +prompt("Enter subject 1 "?);
+
+// // //                                              var sub2 = +prompt("Enter subject 2 ?");
+
+// // //                                              var sub3 = +prompt("Enter subject 3 ?");
+
+// // //                                              var totlamarks = 300;
+
+// // //                                              var marksobtained = sub + sub2 + sub 3 * 100 / 5;
+
+// // //                                              if ( marksobtained <= 60 && >= 50){
+// // //                                                  alert("Sorry you failed");
+// // //                                              }
+
+// // //                                              if ( marksobtained  <= 70 &&  marksobtained >= 60  )
+// // //                                                  alert ("you passed you grade is b");
+// // //                                              }
+
+// // //                                              if (marksobtained <= 90 && marksobtained >= 90  ){
+// // //                                                  alert("yOU PASSED your grade is a+");
+
+// // //                                              }
+
+
+                                             
+                                            
+// //                                    (   If else @ else if Statements  )
+
+
+// //                                               Task - 1
+
+
+// //                                    var age = prompt("ENter you age ?");
+
+// //                                    if (age >= 18){
+// //                                        alert("Your youg");
+// //                                    }
+
+// //                                    else {
+// //                                        alert ("Your too old");
+// //                                    }
+
+
+// //                                                       Task - 2
+
+
+
+
+// //                                                       var username = prompt("enter your name");
+
+// //                                                       if ( username === "Abdul Salam"){
+// //                                                           alert("Your the best !")
+// //                                                       }
+
+
+
+//           Task - 3
+
+
+// var nickname = prompt("Abdul Salam");
+// var firstName = "Ali";
+// if (firstName === "Fahad") {
+// document.write("Hello Fahad!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
